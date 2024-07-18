48ae799695a1d9f27cfbc151fc6c192912fe5d0b
hardik18-hk19 <hardikagarwal9308@gmail.com>
2024-07-19 04:21:21 +0530

    CategoryList add

--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -1,5 +1,30 @@
-const MASTER_URL ="https://ap-south-1.cdn.hygraph.com/content/"+process.env.NEXT_PUBLIC_MASTER_URL_KEY+"/master"
+import { gql, request } from "graphql-request";
 
-const getCategory= async () =>{
-    const query =10
+const MASTER_URL =
+  "https://ap-south-1.cdn.hygraph.com/content/" +
+  process.env.NEXT_PUBLIC_MASTER_URL_KEY +
+  "/master";
+
+const getCategory = async () => {
+  const query = gql`
+    query Category {
+      categories {
+        bgcolor {
+          hex
+        }
+        id
+        name
+        icon {
+          url
+        }
+      }
+    }
+  `;
+
+  const result = await request(MASTER_URL, query);
+  return result;
+};
+
+export default{
+    getCategory
 }
--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
+import CategoryList from "./_components/CategoryList";
 
 export default function Home() {
   return (
     <div>
-     <Hero/>
+      <Hero />
+      <CategoryList />
     </div>
   );
 }
